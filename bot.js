fb4e8527000591e56b78c60333d71722635fe46d
dota1g <alfredtheslugger@gmail.com>
2023-12-22 19:00:13 +0800

    cleaner code

--- bot.js
+++ bot.js
@@ -1,6 +1,10 @@
 require("dotenv").config();
+import { fish_list, animals } from "./data";
 const uname = process.env.USER_NAME;
 const token = process.env.ACCESS_TOKEN;
+const refresh_token = process.env.REFRESH_TOKEN;
+const client_id = process.env.CLIENT_ID;
+const client_secret = process.env.CLIENT_SECRET;
 const tmi = require("tmi.js");
 
 const opts = {
@@ -10,9 +14,18 @@ const opts = {
   },
   channels: ["jahmsd", "sveltebs"],
 };
+// function refreshToken() {
+//   fetch("https://id.twitch.tv/oauth2/token", {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/x-www-form-urlencoded",
+//     },
+//     body: `grant_type=refresh_token&refresh_token=${refresh_token}&client_id=${client_id}&client_secret=${client_secret}`,
+//   }).then((response) => console.log(JSON.stringify(response.json)));
+// }
 
 const client = new tmi.client(opts);
-
+var enabled = true;
 client.on("message", onMessageHandler);
 client.on("connected", onConnectedHandler);
 
@@ -21,102 +34,54 @@ async function onMessageHandler(target, context, msg, self) {
   if (self) {
     return;
   }
-  const commandName = msg.trim();;
+  let ctx = context;
+  console.log(
+    "username:" + ctx.username + " badges: " + JSON.stringify(ctx.badges)
+  );
+  const commandName = msg.trim();
   console.log(commandName);
-  if (commandName === "!dice" || commandName === "!dice 󠀀") {
-    client.ping();
-    const num = rollDice();
-    await client.say(target, `You rolled a ${num} furinaSmug`);
-    console.log(`* Executed ${commandName} command`);
-  } else if (commandName === "!fish" || commandName === "!fish 󠀀") {
-    fishGame(target);
-  } else if (commandName === "hi") {
-    client.say(target, `freen`);
-  } else if (commandName === "buh") {
-    client.say(target, `buh`);
+  if (enabled) {
+    if (commandName === "!dice" || commandName === "!dice 󠀀") {
+      client.ping();
+      const num = rng(6);
+      await client.say(target, `You rolled a ${num} furinaSmug`);
+      console.log(`* Executed ${commandName} command`);
+    } else if (commandName === "!fish" || commandName === "!fish 󠀀") {
+      fishGame(target);
+    } else if (commandName === "hi") {
+      client.say(target, `freen`);
+    } else if (commandName === "buh") {
+      client.say(target, `buh`);
+    } else if (commandName === "!disable" || commandName === "!disable 󠀀") {
+      if (ctx.badges.broadcaster === "1" || ctx.badges.vip === "1") {
+        enabled = false;
+        client.say(target, `Bye!`);
+      } else {
+        return;
+      }
+    } else {
+      client.say(`* Unknown command ${commandName}`);
+    }
   } else {
-    client.say(`* Unknown command ${commandName}`);
+    if (commandName === "!enable" || commandName === "!enable 󠀀") {
+      if (ctx.badges.broadcaster === "1" || ctx.badges.vip === "1") {
+        enabled = true;
+        client.say(target, `I'm back! furinaSmug`);
+      } else {
+        return;
+      }
+    } else {
+      return;
+    }
   }
 }
-
-function rollDice() {
-  const sides = 6;
-  return Math.floor(Math.random() * sides) + 1;
-}
-
-function rng() {
-  const sides = 100;
+function rng(high) {
+  const sides = high;
   return Math.floor(Math.random() * sides) + 1;
 }
 
 function fishGame(target) {
-  const fish_list = [
-    "Anchovy",
-    "Bass (black)",
-    "Bass (striped)",
-    "Bluefish",
-    "Buffalo Fish",
-    "Butterfish",
-    "Calamari (Squid)",
-    "Carp",
-    "Catfish (farm-raised)",
-    "Chilean sea bass",
-    "Clam",
-    "Cod",
-    "Crab (Blue, King and Snow)",
-    "Crayfish (Crawfish)",
-    "Croaker (Atlantic)",
-    "Flounder",
-    "Golden Snapper",
-    "Grouper",
-    "Haddock",
-    "Hake",
-    "Halibut",
-    "Herring",
-    "Jack (Amber, Crevalle)",
-    "Jacksmelt",
-    "King Mackerel",
-    "Lobster (northern, Maine, American)",
-    "Lobster (spiny)",
-    "Mackerel (Atlantic, jack, chub)",
-    "Mahi Mahi (Dolphin-fish)",
-    "Marlin",
-    "Mullet",
-    "Orange Roughy",
-    "Oysters (cooked)",
-    "Perch (Freshwater)",
-    "Perch (Ocean)",
-    "Pickerel",
-    "Plaice",
-    "Pollock",
-    "Pompano (Florida)",
-    "Rainbow Trout (farm-raised)",
-    "Sablefish",
-    "Salmon (wild or farm-raised)",
-    "Sardine",
-    "Scallop",
-    "Scorpion Fish",
-    "Sea Trout",
-    "Shad (American)",
-    "Shark",
-    "Shrimp",
-    "Snapper",
-    "Sole",
-    "Spanish Mackerel (Gulf of Mexico)",
-    "Spanish Mackerel (South Atlantic)",
-    "Squid (Calamari)",
-    "Swordfish",
-    "Tilapia",
-    "Tilefish (Atlantic)",
-    "Tilefish (Gulf of Mexico)",
-    "Tuna",
-    "Walleye (Great Lakes, Canada)",
-    "Weakfish",
-    "White Croaker (Pacific)",
-    "Whitefish",
-    "Whiting",
-  ];
-  rngeesus = rng();
+  rngeesus = rng(100);
   console.log("RNG: " + rngeesus);
   if (rngeesus >= 90) {
     const getFish = fish_list[Math.floor(Math.random() * fish_list.length) + 1];
